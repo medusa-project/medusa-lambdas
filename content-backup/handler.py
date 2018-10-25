bbb3210f9ab4f24afd53f16f43d486147831c2de
Howard Ding <hding2@illinois.edu>
2018-10-25 15:28:10 -0500

    More or less complete attempt at the lambda side, sans size checking Refactoring

--- content-backup/handler.py
+++ content-backup/handler.py
@@ -2,15 +2,22 @@ import os
 import boto3
 import uuid
 import urllib.parse
+import json
+import time
 
-
+# noinspection PyBroadException
 def lambda_handler(event, context):
-  aws_access_key_id = os.environ['ACCESS_KEY_ID']
-  aws_secret_access_key = os.environ['SECRET_ACCESS_KEY']
-  source_bucket = os.environ['SOURCE_BUCKET']
-  target_bucket = os.environ['TARGET_BUCKET']
-  target_region = os.environ['TARGET_REGION']
-  queue_url = os.environ['SQS_URL']
+  params = lambda_params(event)
+  run_uuid = str(uuid.uuid4())
+  send_message('start', run_uuid, params)
+  try:
+    copy_object(params)
+    send_message('end', run_uuid, params)
+  except:
+    send_message('error', run_uuid, params)
+  return 'ok'
+
+def lambda_params(event):
   record = event['Records'][0]
   source_region = record['awsRegion']
   s3_object = record['s3']['object']
@@ -18,30 +25,47 @@ def lambda_handler(event, context):
   #need to fix up the key, which may be uri encoded
   object_key = urllib.parse.unquote_plus(raw_key)
   size = s3_object['size']
-  sqs_client = boto3.client('sqs', aws_access_key_id=aws_access_key_id,
-                            aws_secret_access_key=aws_secret_access_key)
-  runtime_uuid = str(uuid.uuid4())
-  sqs_client.send_message(QueueUrl=queue_url,
-                          MessageBody=runtime_uuid)
-  copy_object(aws_access_key_id, aws_secret_access_key, object_key, source_bucket, source_region, target_bucket, target_region)
+  return {
+    'aws_access_key_id': os.environ['ACCESS_KEY_ID'],
+    'aws_secret_access_key': os.environ['SECRET_ACCESS_KEY'],
+    'source_bucket': os.environ['SOURCE_BUCKET'],
+    'source_region': source_region,
+    'target_bucket': os.environ['TARGET_BUCKET'],
+    'target_region': os.environ['TARGET_REGION'],
+    'queue_url': os.environ['SQS_URL'],
+    'object_key': object_key,
+    'size': size
+  }
 
-  return 'ok'
+def send_message(event_name, run_uuid, params):
+  sqs_client = boto3.client('sqs', aws_access_key_id=params['aws_access_key_id'],
+                            aws_secret_access_key=params['aws_secret_access_key'])
+  sqs_client.send_message(QueueURL=params['QueueUrl'],
+                          MessageBody=message_body(event_name, run_uuid, params['object_key']))
 
+def message_body(event_name, run_uuid, object_key):
+  current_time = int(time.time())
+  message = {
+    'event': event_name,
+    'run_uuid': run_uuid,
+    'object_key': object_key,
+    'time': current_time
+  }
+  return json.dumps(message)
 
-def copy_object(aws_access_key_id, aws_secret_access_key, object_key, source_bucket, source_region, target_bucket,
-                target_region):
+def copy_object(params):
   copy_source = {
-    'Bucket': source_bucket,
-    'Key': object_key
+    'Bucket': params['source_bucket'],
+    'Key': params['object_key']
   }
   client = boto3.client(
     's3',
-    region_name=target_region,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key)
+    region_name=params['target_region'],
+    aws_access_key_id=params['aws_access_key_id'],
+    aws_secret_access_key=params['aws_secret_access_key'])
   source_client = boto3.client(
     's3',
-    region_name=source_region,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key)
-  client.copy(copy_source, target_bucket, object_key, SourceClient=source_client)
+    region_name=params['source_region'],
+    aws_access_key_id=params['aws_access_key_id'],
+    aws_secret_access_key=params['aws_secret_access_key'])
+  client.copy(copy_source, params['target_bucket'], params['object_key'], SourceClient=source_client)
